b96b22cf9fc5f76e4ea0299479c0165d3ff2779d
Mario Macías <mariomac@gmail.com>
2013-09-19 22:39:51 +0200

    Update example.c

--- example.c
+++ example.c
@@ -1,3 +1,9 @@
+/*
+THE BEER-WARE LICENSE
+I, Mario Macías, wrote this file. As long as you retain this notice you
+can do whatever you want with this stuff. If we meet some day, and you think
+this stuff is worth it, you can buy me a beer in return.
+*/
 #include <stdio.h>
 
 #include "tortuga.h"
